ad314ad6d6926d7065e398b4010429966e24a1c5
BrandonNelson-Creator <n.ellyson@hotmail.com>
2021-10-12 10:57:44 -0300

    tried to start

--- Program.cs
+++ Program.cs
@@ -8,12 +8,19 @@ namespace ArraysProject
 {
     class Program
     {
+        static int weapon = 0;
+        static int[] ammo = new int[3];
+        static int[] weaponType = new int[3];
+        
+        
         
 
         static void Main(string[] args)
         {
-            Console.WriteLine("cheese");
-            Console.ReadKey(true);
+            weaponType[0] = 6;
+            weaponType[1] = 10;
+            weaponType[2] = 25;
+            weaponType[3] = 30;
         }
     }
 }
