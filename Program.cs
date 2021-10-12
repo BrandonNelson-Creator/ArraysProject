62f123be55135fc0c6caa100dcfd3e08f6cb35f5
BrandonNelson-Creator <n.ellyson@hotmail.com>
2021-10-12 16:29:44 -0300

    Finished

--- Program.cs
+++ Program.cs
@@ -13,6 +13,9 @@ namespace ArraysProject
        
         static int[] ammoType = new int[weaponAmount];
         static string[] weaponType = new string[weaponAmount];
+        static int[] ammoMaxes = new int[weaponAmount];
+
+        static int maxAmmo;
 
 
 
@@ -32,12 +35,24 @@ namespace ArraysProject
             ammoType[3] = 200;
             ammoType[4] = 25;
 
+            ammoMaxes[0] = ammoType[0];
+            ammoMaxes[1] = ammoType[1];
+            ammoMaxes[2] = ammoType[2];
+            ammoMaxes[3] = ammoType[3];
+            ammoMaxes[4] = ammoType[4];
+
             ShowHud();
             SwitchWeapon(1);
             ShowHud();
             Fire(2);
             ShowHud();
-            Reload(ammoType[1] + weaponAmount);
+            Reload(10);
+            ShowHud();
+            SwitchWeapon(4);
+            ShowHud();
+            Fire(5);
+            ShowHud();
+            Reload(5);             //Reload(100); tried to break code.
             ShowHud();
             Console.ReadKey(true);
 
@@ -46,26 +61,38 @@ namespace ArraysProject
 
         static void ShowHud()
         {
-            Console.WriteLine(weaponType[weapon] + ": " + ammoType[weapon]);     
+            Console.WriteLine();
+            Console.WriteLine("Player weapon: " + weaponType[weapon]);
+            Console.WriteLine("Ammo for " + weaponType[weapon] + ": " + ammoType[weapon]);
+            Console.WriteLine();
             
         }
 
         static void SwitchWeapon(int weaponToSwitch)
         {
-            weapon = weaponToSwitch - 1;
+            weapon = weaponToSwitch;
             Console.WriteLine("Player switched to " + weaponType[weapon]);
         }
 
         static void Fire(int amount)
         {
             ammoType[weapon] = ammoType[weapon] - amount;
-            Console.WriteLine("Player fired " + weaponType[weapon]);
+            if (ammoType[weapon] <= 0)
+            {
+                ammoType[weapon] = 0;
+            }
+            Console.WriteLine("Player fired " + amount + " " + weaponType[weapon] + " bullets");
         }
 
         static void Reload(int amount)
         {
+         
             ammoType[weapon] = ammoType[weapon] + amount;
-            Console.WriteLine("Player is reloading " + ammoType[weapon]);
+            if (ammoType[weapon] >= ammoMaxes[weapon])
+            {
+                ammoType[weapon] = ammoMaxes[weapon];
+            }
+            Console.WriteLine("Player is reloading " + ammoType[weapon] + " bullets for " + weaponType[weapon]);
         }
     }
 }
